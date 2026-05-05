8b6485ac20477646abe19c5e634f230b28adbea7
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-05 09:54:14 +0700

    responsive f1

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const header = document.querySelector(".header");
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+const menu = document.querySelector("#menu-icon");
+const navMenu = document.querySelector(".navmenu");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navMenu.classList.toggle("open");
+};
